76946bbfd30a789725fa715de28589f04a7f3721
singhnavneet77 <143984860+singhnavneet77@users.noreply.github.com>
2023-11-03 23:02:41 +0530

    true

--- TT@21binerysearch.c
+++ TT@21binerysearch.c
@@ -1,46 +1,83 @@
+// #include<stdio.h>
+// int binary(int arr[],int key,int f,int l){ //Here code run using recursion.
+//   int m ;
+//   if(f<=l){
+//     m=(f+l)/2;
+//     if(arr[m]==key)
+//     return m;
+//     else if(key<arr[m])
+//     return binary(arr,key,f,m-1);
+//     else
+//     return binary(arr,key,m+1,l);
+//   }
+//   return -1;
+// }
+//Space complixity=0(1);In recursion=0(n); //Time complixity=log(n);
+// int main(){
+//   int n,key,c=0,i,f,l,m;
+//   printf("Enter size:");
+//   scanf("%d",&n);
+//   int arr[n];
+//   l=n-1;
+//   f=0;
+//   printf("Enter array element:");
+//   for(i=0;i<n;i++){
+//   scanf("%d",&arr[i]);}
+//   printf("Enter key element:");
+//   scanf("%d",&key);
+//    c=binary(arr,key,f,l);
+//  printf("Element found at %d",c);
+//    return 0;
+// }
+// int binary(int arr[],int key,int f,int l){   //Here code run using iteration.
+// int m;
+//   while(f<=l){
+//     m=(f+l)/2;
+//   if(arr[m]==key){
+//     return m;
+//   }
+//   else if(arr[m]>key)
+//   l=m-1;
+//   else
+//   f=m+1;
+// }
+// return -1;
+// }
+
+
+//Here code start using stracture.
 #include<stdio.h>
-int binary(int arr[],int key,int f,int l);//Space complixity=0(1);In recursion=0(n);
-int main(){                        //Time complixity=log(n);
-  int n,key,c=0,i,f,l,m;
-  printf("Enter size:");
-  scanf("%d",&n);
-  int arr[n];
-  l=n-1;
-  printf("Enter array element:");
-  for(i=0;i<n;i++){
-  scanf("%d",&arr[i]);}
-  printf("Enter key element:");
-  scanf("%d",&key);
-   c=binary(arr,key,f,l);
- printf("Element found at %d",c);
-   return 0;
+struct student{
+  int array[5];
+  int length;
+};
+int search(struct student arr,int key,int f,int l);
+void main(){
+struct student arr={{1,2,3,4,5},5};
+int key,f,l;
+f=0;
+l=arr.length-1;
+printf("Enter key: ");
+scanf("%d",&key);
+int temp;
+temp = search(arr,key,f,l);
+if (temp != -1) {
+       printf("Element found at index: %d\n", temp);
+   } else {
+       printf("Element not found.\n");
+   }
 }
-int binary(int arr[],int key,int f,int l){
-int m;
-  while(f<=l){
-    m=(f+l)/2;
-  if(arr[m]==key){
-    return m;
-  }
-  else if(arr[m]>key)
-  l=m-1;
-  else
-  f=m+1;
-}
-return -1;
-}
-/*int binary(int arr[],int key,int f,int l){
-  int m ;
-  //m=(f+l)/2;
-  while(f<=l){
-    m=(f+l)/2;
-    if(arr[m]==key)
-    return m;
-    else if(key<arr[m])
-    return binary(arr,key,f,m-1);
-    else
-    return binary(arr,key,m+1,l);
-  }
+int search(struct student arr,int key,int f,int l){
+ int m;
+  if(f<=l){
+   m=(f+l)/2;
+if(arr.array[m]==key){
+return m;
+   }
+else if(arr.array[m]>key)
+    l=m-1;
+   else
+f=m+1;
+   }
   return -1;
-}
-*/
+  }
--- TT@25Occerance.c
+++ TT@25Occerance.c
@@ -1,37 +1,59 @@
-#include<stdio.h>
-// int oc(int a[],int f,int l,int s){
+// #include<stdio.h>
+// int Firstoc(int a[],int f,int l,int s){
 //   int i,m;
 //   m=(f+l)/2;
 //   if(f<=l){
 //     if(a[m]==s){
 //     if(a[m-1]==s&&m-1>=0)
-//     return oc(a,f,l-1,s);
+//     return Firstoc(a,f,l-1,s);
 //     else
 //     return m;
 //   }
 //     else if(a[m]>s)
-//     return oc(a,m+1,l,s);
-//     else if(a[m]<s)
-//     return oc(a,f,m-1,s);
+//     return Firstoc(a,f,m-1,s);
+//     else
+//     return Firstoc(a,m+1,l,s);
 //   }
 //   return -1;
 // }
-int oc(int a[],int f,int l,int s){
+// int main(){
+//   int n,i,f,l,key,c=0;
+//   printf("Enter size:");
+//   scanf("%d",&n);
+//   int a[n];
+//   l=n-1;
+//   printf("Enter array element:");
+//   for(i=0;i<n;i++){
+//     scanf("%d",&a[i]);
+//   }
+//   printf("Enter key element:");
+//   scanf("%d",&key);
+//   c = Firstoc(a,f,l,key);
+//   printf("First occurance is %d",c);
+//   return 0;
+// }
+
+//This code print last occurance.
+
+#include<stdio.h>
+int Lastoc(int a[],int f,int l,int s){
   int i,m;
   m=(f+l)/2;
   if(f<=l){
     if(a[m]==s){
     if(a[m+1]==s&&m+1<=l)
-    return oc(a,f,l+1,s);
+    return Lastoc(a,f,l+1,s);
     else
     return m;
   }
     else if(a[m]>s)
-    return oc(a,m+1,l,s);
+    return Lastoc(a,f,m-1,s);
     else if(a[m]<s)
-    return oc(a,f,m-1,s);
+    return Lastoc(a,m+1,l,s);
   }
-  return -1;}
+  return -1;
+}
+
 int main(){
   int n,i,f,l,key,c=0;
   printf("Enter size:");
@@ -44,7 +66,7 @@ int main(){
   }
   printf("Enter key element:");
   scanf("%d",&key);
-  c = oc(a,f,l,key);
-  printf("First occurance is %d",c);
+  c = Lastoc(a,f,l,key);
+  printf("Last occurance is %d",c);
   return 0;
 }
